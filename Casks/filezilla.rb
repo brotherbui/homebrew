f76f7555705c1eae4a8a2c2ce90367aea81202ad
Brother Bui <buiphong@me.com>
2022-10-17 16:13:14 +0700

    Create filezilla.rb

---
+++ Casks/filezilla.rb
@@ -0,0 +1,17 @@
+cask "filezilla" do
+  version "3.61.0"
+  sha256 :no_check # required as upstream package is updated in-place
+
+  url "https://dl2.cdn.filezilla-project.org/client/FileZilla_#{version}_macosx-x86.app.tar.bz2"
+  name "FileZilla"
+  desc "FileZilla®, the free FTP solution"
+  homepage "https://filezilla-project.org"
+
+  auto_updates true
+
+  app "FileZilla.app"
+
+  zap trash: [
+    "~/Library/Application Support/FileZilla",
+  ]
+end
