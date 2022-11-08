49a533c6267166e8216c6e2431ffce788ecc831d
Brother Bui <buiphong@me.com>
2022-11-08 17:17:10 +0700

    Update magerun.rb

--- Formula/magerun.rb
+++ Formula/magerun.rb
@@ -3,7 +3,7 @@ class Magerun < Formula
   desc "Magerun"
   homepage "https://magerun.net"
   url "https://files.magerun.net/n98-magerun.phar"
-  sha256 :no_check # required as upstream package is updated in-place
+  #sha256 :no_check # required as upstream package is updated in-place
   license "Apache"
 
 
