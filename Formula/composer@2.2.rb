b327536ade4b48048b14b707599b77031b33781c
Brother Bui <buiphong@me.com>
2023-01-04 07:06:24 +0700

    Update composer@2.2.rb

--- Formula/composer@2.2.rb
+++ Formula/composer@2.2.rb
@@ -1,4 +1,4 @@
-class Composer2 < Formula
+class ComposerAT22 < Formula
   desc "Dependency Manager for PHP 7"
   homepage "https://getcomposer.org/"
   url "https://getcomposer.org/download/2.2.18/composer.phar"
