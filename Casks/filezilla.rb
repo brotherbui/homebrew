426df6b30a54626660b21059302f9d8e875bd2ab
Brother Bui <buiphong@me.com>
2023-01-25 21:02:33 +0700

    Update filezilla.rb

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,8 +1,8 @@
 cask "filezilla" do
-  version "3.61.0"
+  version "3.63.0"
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://dl2.cdn.filezilla-project.org/client/FileZilla_#{version}_macosx-x86.app.tar.bz2?h=IItpPhL-eAnVAjMfFUK9_g&x=1666001729"
+  url "https://dl2.cdn.filezilla-project.org/client/FileZilla_3.63.0_macosx-x86.app.tar.bz2?h=8C6Wbyz2omXW21v3gEYZdA&x=1674658897"
   name "FileZilla"
   desc "FileZilla®, the free FTP solution"
   homepage "https://filezilla-project.org"
