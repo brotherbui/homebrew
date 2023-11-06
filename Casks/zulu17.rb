713f98fa5252c120b2c1b1ee425f79616ce3de4d
Brother Bui <buiphong@me.com>
2023-11-06 13:14:51 +0700

    update

--- Casks/zulu17.rb
+++ Casks/zulu17.rb
@@ -2,9 +2,9 @@ cask "zulu17" do
   arch arm: "aarch64", intel: "x64"
   choice = on_arch_conditional arm: "arm", intel: "x86"
 
-  version "17.0.8,17.44.15-ca"
-  sha256 arm:   "b939350c5901fd8027356d03ecd2614526a1f682a4cf030ab440aa50b3165ef5",
-         intel: "d8e2e67b7a814062fb34309a6dc2b636f8d062714451ea76ccb56639d46fc1f6"
+  version "17.0.9,17.46.19-ca"
+  sha256 arm:   "cec93e9bb5df6a77885d7b012c854a69ef5b4ba75e3db36e77b9df3aca149222",
+         intel: "9a24939bfc5df574675c4d666d78a3211f00411cc08b4872dfc07a0bf7e5fd90"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
@@ -14,7 +14,7 @@ cask "zulu17" do
 
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=false&ext=dmg&os=macos&arch=#{choice}"
-    regex(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
+    regex(/zulu(\d+(?:[._]\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
     end
--- Casks/zulu21.rb
+++ Casks/zulu21.rb
@@ -1,24 +1,26 @@
-cask "zulu11" do
+cask "zulu21" do
   arch arm: "aarch64", intel: "x64"
   choice = on_arch_conditional arm: "arm", intel: "x86"
 
-  version "11.0.21,11.68.17-ca"
-  sha256 arm:   "db2d52eec86d62ad42fc6a22e3f165d99070d77a98e55f7c45c5d07ae2f194b6",
-         intel: "4bdb01ee04b804438e5c567a078da60e130e93ca2eec1739489876de2ea1a9b4"
+  version "21.0.1,21.30.15-ca"
+  sha256 arm:   "92d65ed61bf6c0bea0f9c44a6dd538da8a6c620a78f517c194913d1338fc39e7",
+         intel: "f8f46bb00cfb8cecddcf71e10c77a577c9e9843a1e13a35ff05dbeec6f29788f"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   name "Azul Zulu Java Standard Edition Development Kit"
   desc "OpenJDK distribution from Azul"
-  homepage "https://www.azul.com/products/core/"
+  homepage "https://www.azul.com/downloads/"
 
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=false&ext=dmg&os=macos&arch=#{choice}"
-    regex(/zulu(\d+(?:[._]\d+)*-.*?)-jdk(\d+(?:\.\d+)+)-macosx_#{arch}\.dmg/i)
+    regex(/zulu(\d+(?:[._]\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
     end
   end
+
+  depends_on macos: ">= :big_sur"
 
   pkg "Double-Click to Install Azul Zulu JDK #{version.major}.pkg"
 
