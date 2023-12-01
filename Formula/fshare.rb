1b3939d6aab4a4a6f400f7c1cdc673690f5557fb
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2023-12-01 21:00:07 +0700

    Update fshare.rb

--- Formula/fshare.rb
+++ Formula/fshare.rb
@@ -12,7 +12,11 @@ class Fshare < Formula
   end
 
   service do
-    run [bin/"aria2c", "--enable-rpc=true", "--rpc-secret=@1nonmihcn0@", "--rpc-allow-origin-all=true", "--rpc-listen-all=true", "--rpc-listen-port=6800"]
+    if Hardware::CPU.arm?
+      run ["/opt/homebrew/bin/aria2c", "--enable-rpc=true", "--rpc-secret=@1nonmihcn0@", "--rpc-allow-origin-all=true", "--rpc-listen-all=true", "--rpc-listen-port=6800"]
+    else
+      run ["/usr/local/bin/aria2c", "--enable-rpc=true", "--rpc-secret=@1nonmihcn0@", "--rpc-allow-origin-all=true", "--rpc-listen-all=true", "--rpc-listen-port=6800"]
+    end
     keep_alive true
   end
 end
