5680dcc44b16f7f97eb18c7abd4fd9d0149e6143
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2023-12-01 19:50:38 +0700

    Update fshare.rb

--- Formula/fshare.rb
+++ Formula/fshare.rb
@@ -4,7 +4,7 @@ class Fshare < Formula
   url "https://phongblack.me/media/fshare.tar.gz"
   sha256 "6e47d89151148b325425076c829e855d4b44de45e6345da11821b641a4a831c4"
   license "EPL-2.0"
-
+  version "1.0"
   depends_on "aria2"
 
   def install
