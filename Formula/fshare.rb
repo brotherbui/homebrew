d8b1ef449ed1be503592b712973ac4ed91b211c1
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2023-12-01 22:23:11 +0700

    Update fshare.rb

--- Formula/fshare.rb
+++ Formula/fshare.rb
@@ -1,10 +1,19 @@
 class Fshare < Formula
   desc "Overkill Fshare downloader"
   homepage "https://phongblack.me"
-  url "https://phongblack.me/media/fshare.tar.gz"
-  sha256 "6e47d89151148b325425076c829e855d4b44de45e6345da11821b641a4a831c4"
   license "EPL-2.0"
-  version "1.0"
+  version "1.0.0"
+
+  on_intel do
+    url "https://phongblack.me/media/fshare-intel.tar.gz"
+    sha256 "e1a4916b85797db584aadbed75015aad38342e035b134aea2249a7280e90c44f"
+  end
+
+  on_arm do
+    url "https://phongblack.me/media/fshare-arm.tar.gz"
+    sha256 "d5ba04ca40e8e916e1bced9329e1cf3f39e1fa3e31f333db07b7de32a3eae20f"
+  end
+
   depends_on "aria2"
 
   def install
@@ -12,14 +21,11 @@ class Fshare < Formula
   end
 
   service do
-    depends_on :macos
-    on_arm do
-      run ["/opt/homebrew/bin/aria2c", "--enable-rpc=true", "--rpc-secret=@1nonmihcn0@", "--rpc-allow-origin-all=true", "--rpc-listen-all=true", "--rpc-listen-port=6800"]
+    if Hardware::CPU.arm?
+      run ["/opt/homebrew/bin/aria2c", "--enable-rpc=true", "--rpc-secret=welovephongblack", "--rpc-allow-origin-all=true", "--rpc-listen-all=true", "--rpc-listen-port=6800"]
+    else
+      run ["/usr/local/bin/aria2c", "--enable-rpc=true", "--rpc-secret=welovephongblack", "--rpc-allow-origin-all=true", "--rpc-listen-all=true", "--rpc-listen-port=6800"]
     end
-    on_intel do
-      run ["/usr/local/bin/aria2c", "--enable-rpc=true", "--rpc-secret=@1nonmihcn0@", "--rpc-allow-origin-all=true", "--rpc-listen-all=true", "--rpc-listen-port=6800"]
-    end
-    
     keep_alive true
   end
 end
