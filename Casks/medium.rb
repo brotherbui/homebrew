b400bf0a743179b56c89e5f4d657a9129b42fb8a
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-03-31 11:11:20 +0700

    refactor

---
+++ Casks/medium.rb
@@ -0,0 +1,17 @@
+cask "medium" do
+  version "1.1.68"
+  sha256 "848e1f7f7ec8e9cb61c5bf42f7950b29a977e6125f710cd504e434c38e067fe1"
+
+  url "https://phongblack.me/media/medium-readable.zip"
+  name "Medium Readable"
+  desc "Medium Memeber-only Unlocker"
+  homepage "https://phongblack.me"
+
+  auto_updates true
+
+  app "Medium Readable.app"
+
+  zap trash: [
+    "~/Library/Application Support/Medium Readable",
+  ]
+end
