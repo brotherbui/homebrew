ae67d1590f962b2993d8a6740cd9ac142809f963
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-05-25 18:23:30 +0700

    Update medium.rb

--- Casks/medium.rb
+++ Casks/medium.rb
@@ -2,7 +2,7 @@ cask "medium" do
   version "1.1.68"
   sha256 "848e1f7f7ec8e9cb61c5bf42f7950b29a977e6125f710cd504e434c38e067fe1"
 
-  url "https://phongblack.me/media/medium-readable.zip"
+  url "https://github.com/brotherbui/public/raw/main/medium-readable.zip"
   name "Medium Readable"
   desc "Medium Memeber-only Unlocker"
   homepage "https://phongblack.me"
