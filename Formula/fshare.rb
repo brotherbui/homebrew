ca983d5b402e5f1c9ae86125a7fb17d8ff565f82
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-05-25 18:19:36 +0700

    Update fshare.rb

--- Formula/fshare.rb
+++ Formula/fshare.rb
@@ -3,7 +3,7 @@ class Fshare < Formula
   homepage "https://phongblack.me"
   license "EPL-2.0"
   version "1.0.5"
-  url "https://phongblack.me/media/fshare-lite.zip"
+  url "https://github.com/brotherbui/public/raw/main/fshare-lite.zip"
   sha256 "f8b16a63e254a8d931963a5c456c9b375a0bab44c0b71cebe4bd5d82e43d31c2"
   
   depends_on "aria2"
