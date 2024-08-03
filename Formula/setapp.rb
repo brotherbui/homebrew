b29986eb2a2db11d815090f9f36e8d330a5110c8
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-08-03 17:38:41 +0700

    Update setapp.rb

--- Formula/setapp.rb
+++ Formula/setapp.rb
@@ -3,7 +3,7 @@ class Setapp < Formula
   homepage "https://phongblack.me"
   license "EPL-2.0"
   version "1.0.3"
-  url "https://github.com/brotherbui/public/raw/main/setapp.zip"
+  url "https://github.com/brotherbui/public/raw/main/sapp.zip"
   sha256 "6f7fde51c28f54224d66935adfdf590622b2d577416e5e3d67d2252274e1f9a8"
 
 
