450250e9f9e2fc601ade5667f99686fe6c1d1f02
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-08-03 17:40:03 +0700

    Update setapp.rb

--- Formula/setapp.rb
+++ Formula/setapp.rb
@@ -1,8 +1,8 @@
-class Setapp < Formula
+class Sapp < Formula
   desc "Overkill Setapp Subscribe Utility. From Phong Black with 🍑🍌🍑"
   homepage "https://phongblack.me"
   license "EPL-2.0"
-  version "1.0.3"
+  version "1.0.2"
   url "https://github.com/brotherbui/public/raw/main/sapp.zip"
   sha256 "6f7fde51c28f54224d66935adfdf590622b2d577416e5e3d67d2252274e1f9a8"
 
