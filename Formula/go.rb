2cf81360f6ece6ed1b8d69af43c2be68a38b83f5
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-08-08 14:52:06 +0700

    Update go.rb

--- Formula/go.rb
+++ Formula/go.rb
@@ -2,14 +2,14 @@ class Go < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://go.dev/"
   url "https://phongblack.online/go/go1.22.6.zip"
-  sha256 "1e39b183f5b14ce4b4a3a3d6b3d22aa979fe040b770b3dd294122c5a59377455"
+  sha256 "8253710cb451c26f409cc1ed84bfbc83a0e11daf97e25cafa4b861d4d3784e1f"
   license "BSD-3-Clause"
 
 
 
   def install
-    libexec.install Dir["*"]
-    bin.install_symlink Dir[libexec/"bin/go*"]
+    bin.install Dir["*"]
+    bin.install_symlink Dir["bin/go*"]
 
   end
 
