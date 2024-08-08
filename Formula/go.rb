210d66b2de8d0cada7976a19d86413a6949cef3d
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-08-08 14:58:38 +0700

    Update go.rb

--- Formula/go.rb
+++ Formula/go.rb
@@ -12,46 +12,4 @@ class Go < Formula
     bin.install_symlink Dir[libexec/"bin/go*"]
 
   end
-
-  test do
-    (testpath/"hello.go").write <<~EOS
-      package main
-
-      import "fmt"
-
-      func main() {
-          fmt.Println("Hello World")
-      }
-    EOS
-
-    # Run go fmt check for no errors then run the program.
-    # This is a a bare minimum of go working as it uses fmt, build, and run.
-    system bin/"go", "fmt", "hello.go"
-    assert_equal "Hello World\n", shell_output("#{bin}/go run hello.go")
-
-    with_env(GOOS: "freebsd", GOARCH: "amd64") do
-      system bin/"go", "build", "hello.go"
-    end
-
-    (testpath/"hello_cgo.go").write <<~EOS
-      package main
-
-      /*
-      #include <stdlib.h>
-      #include <stdio.h>
-      void hello() { printf("%s\\n", "Hello from cgo!"); fflush(stdout); }
-      */
-      import "C"
-
-      func main() {
-          C.hello()
-      }
-    EOS
-
-    # Try running a sample using cgo without CC or CXX set to ensure that the
-    # toolchain's default choice of compilers work
-    with_env(CC: nil, CXX: nil) do
-      assert_equal "Hello from cgo!\n", shell_output("#{bin}/go run hello_cgo.go")
-    end
-  end
 end
