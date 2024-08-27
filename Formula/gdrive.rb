c8820a24e6396a66ade6ef2f8f1e6063f4173860
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-08-27 15:38:39 +0700

    Update gdrive.rb

--- Formula/gdrive.rb
+++ Formula/gdrive.rb
@@ -4,7 +4,7 @@ class Gdrive < Formula
   url "https://github.com/brotherbui/public/raw/main/gdrive.zip"
   sha256 "e0ee80cb260e95ac88279470cde9c0d41a588acbf98b524652eaa2a14778c82a"
   license "MIT"
-  head "https://github.com/glotlabs/gdrive.git", branch: "main"
+  version "3.9.1"
 
   def install
     bin.install "./gdrive"
