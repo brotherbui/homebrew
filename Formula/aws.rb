425f6dac58ffb95f15bbaa52fc4f28414d6df579
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-09-22 22:15:09 +0700

    Update aws.rb

--- Formula/aws.rb
+++ Formula/aws.rb
@@ -2,10 +2,10 @@ class Aws < Formula
   desc "AWS Command Line Interface"
   homepage "https://aws.amazon.com/cli/"
   url "https://awscli.amazonaws.com/awscli.tar.gz"
-  version "2.7.56"
+  version "2.17.56"
 
   bottle do
-    root_url "https://github.com/brotherbui/homebrew/releases/download/v2.7.56"
+    root_url "https://github.com/brotherbui/homebrew/releases/download/v2.17.56"
     rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "5c4615a93a27d76df9d73dc9d813dcff1c826ca6ffa5de4f4bc07d424d2ebe8c"
   end
