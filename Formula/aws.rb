df3c334e2ab378b743e76aef4cf693fecf43a4ac
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-09-22 19:54:40 +0700

    Create aws.rb

---
+++ Formula/aws.rb
@@ -0,0 +1,20 @@
+class Aws < Formula
+  desc "AWS Command Line Interface"
+  homepage "https://aws.amazon.com/cli/"
+  url "https://awscli.amazonaws.com/awscli.tar.gz"
+  sha256 no_check
+
+  def install
+    system "./configure", "--with-download-deps",
+                          "--with-install-type=portable-exe",
+                          "--prefix=#{prefix}",
+                          "--libdir=#{lib}",
+                          "--bindir=#{bin}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/aws", "--version"
+  end
+end
