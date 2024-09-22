b2d9fd4fb1a682a49511df4d9607901fd1d9da6b
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-09-22 21:24:39 +0700

    Update aws.rb

--- Formula/aws.rb
+++ Formula/aws.rb
@@ -4,6 +4,12 @@ class Aws < Formula
   url "https://awscli.amazonaws.com/awscli.tar.gz"
   version "2.7.56"
 
+  bottle do
+    root_url "https://github.com/brotherbui/homebrew/releases/download/v2.7.56/aws--2.7.56.arm64_sequoia.bottle.1.tar.gz"
+    rebuild 1
+    sha256 arm64_sequoia: "5c4615a93a27d76df9d73dc9d813dcff1c826ca6ffa5de4f4bc07d424d2ebe8c"
+  end
+  
   def install
     system "./configure", "--with-download-deps",
                           "--with-install-type=portable-exe",
