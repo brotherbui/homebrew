5d0676952e6f19cd1af043a5d596f6a66c8afc10
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-10-16 13:16:16 +0700

    Update localserver.rb

--- Formula/localserver.rb
+++ Formula/localserver.rb
@@ -4,14 +4,14 @@ class Localserver < Formula
   license "EPL-2.0"
   version "1.0.0"
   url "https://github.com/brotherbui/public/raw/main/localserver.zip"
-  sha256 "b2cffea43d670eaeba16956958abf881c3115fddc7263b73505c5ba8eb64fb55"
+  sha256 "7e4f93ef3dd07453406837df370e3e707721b409b3d63fa8305da1899ae78a15"
 
   def install
     bin.install "./localserver"
   end
 
   service do
-    run [opt_bin/"localserver"]
+    run [bin/"localserver"]
     keep_alive true
     log_path var/"log/localserver.log"
     error_log_path var/"log/localserver.error.log"
