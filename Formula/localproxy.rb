b6c53a06dfdf4668de639394e29ec4ed5d9133b1
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-10-18 14:48:39 +0700

    Update localproxy.rb

--- Formula/localproxy.rb
+++ Formula/localproxy.rb
@@ -2,9 +2,9 @@ class Localproxy < Formula
   desc "Overkill local reserve proxy"
   homepage "https://phongblack.me"
   license "EPL-2.0"
-  version "1.0.0"
+  version "1.0.1"
   url "https://github.com/brotherbui/public/raw/main/localproxy.zip"
-  sha256 "00639885f274a00e42323327aac34ef780fe3b798463ac2fccf683b0f9d31c71"
+  sha256 "e40f579a94af68c24e502d44d0499e59cfe6a9a955aad275aa2c79e98253244c"
 
   def install
     bin.install "./localproxy"
