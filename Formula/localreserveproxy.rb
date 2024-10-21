742a2c637df311821edf540da807052628943ffb
Brother Bui <80878082+brotherbui@users.noreply.github.com>
2024-10-21 07:27:59 +0700

    Update localreserveproxy.rb

--- Formula/localreserveproxy.rb
+++ Formula/localreserveproxy.rb
@@ -3,8 +3,8 @@ class Localreserveproxy < Formula
   homepage "https://phongblack.me"
   license "EPL-2.0"
   version "1.0.3"
-  url "https://github.com/brotherbui/public/raw/main/localreserveproxy-1.0.3.zip"
-  sha256 "0c6182134409b921981231396421d7620d9338e350646ea740c69615a6386e67"
+  url "https://github.com/brotherbui/public/raw/main/localreserveproxy-1.0.4.zip"
+  sha256 "1b77f89f9cd0bf3afd577163e7fcdb83c2a9e5e01a484d4ce4bf2755a4fd60c8"
 
   def install
     bin.install "./localreserveproxy"
