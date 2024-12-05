78b80a046c94ee540ceea57ead48d01bcb2bfc7f
Brother Bui <buiphong@me.com>
2024-12-06 05:11:34 +0700

    Update fshare.rb

--- Formula/fshare.rb
+++ Formula/fshare.rb
@@ -2,9 +2,19 @@ class Fshare < Formula
   desc "Overkill Fshare downloader"
   homepage "https://phongblack.me"
   license "EPL-2.0"
-  version "1.0.6"
-  url "https://github.com/brotherbui/public/releases/download/fshare/fshare.tgz"
-  sha256 "6fb98ebfb4fe24943416fb78779be5d8b5bc41690a5f667afa3472e080eae25e"
+  version "1.0.7"
+
+
+  on_macos do
+    on_arm do
+        url "https://github.com/brotherbui/public/releases/download/fshare/fshare.tgz"
+        sha256 "6fb98ebfb4fe24943416fb78779be5d8b5bc41690a5f667afa3472e080eae25e"
+    end
+    on_intel do
+        url "https://github.com/brotherbui/public/releases/download/fshare/fshare-intel.tgz"
+        sha256 "f595942db202e179e2732a6c91bf0e1579dfe7008a3dae65dbb887060db574df"
+    end
+  end
 
   depends_on "aria2"
 
