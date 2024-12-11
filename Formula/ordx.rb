981e279f9a744851f72ec73c0355d28f39af83b9
Brother Bui <buiphong@me.com>
2024-12-11 10:55:10 +0700

    Update ordx.rb

--- Formula/ordx.rb
+++ Formula/ordx.rb
@@ -2,26 +2,26 @@ class Ordx < Formula
   desc "Index, block explorer, and command-line wallet. From Phong Black with 🍑🍌🍑 "
   homepage "https://ordinals.com/"
   license "CC0-1.0"
-  version "0.21.2"
+  version "0.22.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/brotherbui/public/releases/download/ord/ordx-0.21.2-macos.tgz"
-      sha256 "256618a879ffa46865037d875324e707965b791ee6c3b459bd9c0df7693f977f"
+      url "https://github.com/brotherbui/public/releases/download/ord/ord.tgz"
+      sha256 "3305fe98b3aaa8c0072d1e9b4a01c3097c147217332341994ef3b0bbf15ca12a"
     end
     on_intel do
-      url "https://github.com/brotherbui/public/releases/download/ord/ordx-0.21.2-macos.tgz"
-      sha256 "256618a879ffa46865037d875324e707965b791ee6c3b459bd9c0df7693f977f"
+      url "https://github.com/brotherbui/public/releases/download/ord/ord.tgz"
+      sha256 "3305fe98b3aaa8c0072d1e9b4a01c3097c147217332341994ef3b0bbf15ca12a"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/brotherbui/public/releases/download/ord/ordx-0.21.2-linux.tgz"
-      sha256 "9932a240d0e242252615f531cfc76aa7ae1cf892454ebfa1ed32387926f0ad4b"
+      url "https://github.com/brotherbui/public/releases/download/ord/ord-linux.tgz"
+      sha256 "28be1a3481208fab02891dfa4b9ba263e9828a4432f2173286b7e6b443d1f82e"
     elsif Hardware::CPU.arm?
-      url "https://github.com/brotherbui/public/releases/download/ord/ordx-0.21.2-linux.tgz"
-      sha256 "9932a240d0e242252615f531cfc76aa7ae1cf892454ebfa1ed32387926f0ad4b"
+      url "https://github.com/brotherbui/public/releases/download/ord/ord-linux.tgz"
+      sha256 "28be1a3481208fab02891dfa4b9ba263e9828a4432f2173286b7e6b443d1f82e"
     end
   end
 
