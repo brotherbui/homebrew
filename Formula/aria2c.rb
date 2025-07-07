c0f56c2776599c112ee87bbcc803e6131c7a37fd
Brother Bui <buiphong@me.com>
2025-07-07 11:16:03 +0700

    Create aria2c.rb

---
+++ Formula/aria2c.rb
@@ -0,0 +1,56 @@
+class Aria2c < Formula
+  desc "Download with resuming and segmented downloading"
+  homepage "https://aria2.github.io/"
+  url "https://github.com/aria2/aria2/releases/download/release-1.37.0/aria2-1.37.0.tar.xz"
+  sha256 "60a420ad7085eb616cb6e2bdf0a7206d68ff3d37fb5a956dc44242eb2f79b66b"
+  license "GPL-2.0-or-later"
+
+  no_autobump! because: :requires_manual_review
+
+
+
+  depends_on "pkgconf" => :build
+  depends_on "libssh2"
+  depends_on "openssl@3"
+  depends_on "sqlite"
+
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "gettext"
+  end
+
+  def install
+    ENV.cxx11
+
+    args = %w[
+      --disable-silent-rules
+      --with-libssh2
+      --without-gnutls
+      --without-libgmp
+      --without-libnettle
+      --without-libgcrypt
+    ]
+    if OS.mac?
+      args << "--with-appletls"
+      args << "--without-openssl"
+    else
+      args << "--without-appletls"
+      args << "--with-openssl"
+    end
+
+    system "./configure", *args, *std_configure_args
+    system "make", "install"
+
+    bash_completion.install "doc/bash_completion/aria2c"
+  end
+  service do
+    run [opt_bin/"aria2c", "--disk-cache=128M", "--enable-rpc=true", "--rpc-secret=welovephongblack", "--rpc-allow-origin-all=true", "--rpc-listen-all=true", "--rpc-listen-port=6800"]
+    keep_alive true
+  end
+  test do
+    system bin/"aria2c", "https://brew.sh/"
+    assert_path_exists testpath/"index.html", "Failed to create index.html!"
+  end
+end
